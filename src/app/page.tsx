9673710eb4127274c4a3865041843e0c3e8171d6
ArpaAP <arpa1705@gmail.com>
2025-11-23 14:17:32 +0900

    feat: 루트 라우트 정책 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
-export default function HomePage() {
-  return <div>Hello, World!</div>;
+import { auth } from '@/lib/auth';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+export default async function HomePage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session) {
+    return redirect('/dashboard');
+  } else {
+    return redirect('/onboard');
+  }
 }
